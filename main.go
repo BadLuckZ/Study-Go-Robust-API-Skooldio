92e3fd952732d9c6769277e2ef1383c7eb2c42a1
BadLuckZ <thanagorn8802@gmail.com>
2026-05-06 22:41:59 +0700

    chore: graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"syscall"
+	"time"
+
+	"os/signal"
 
 	"github.com/BadLuckZ/Study-Go-Robust-API-Skooldio/auth"
 	"github.com/BadLuckZ/Study-Go-Robust-API-Skooldio/todo"
@@ -57,5 +63,41 @@ func main() {
 	// Private
 	protected.POST("/todos", handler.NewTask)
 
-	r.Run()
+	// ===================
+	// Graceful Shutdown
+
+	// Ctrl+C (SIGINT) to kill processes (SIGTERM)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	s := &http.Server{
+		Addr:           ":" + os.Getenv("PORT"),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen: %s\n", err)
+		}
+	}()
+
+	// Not go after this line until Ctrl+C
+	<-ctx.Done()
+	stop()
+	fmt.Println("Shut down gracefully. Ctrl+C again to force")
+
+	// Wait for 5 seconds before real shutdown
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = s.Shutdown(timeoutCtx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// ===================
+
 }
